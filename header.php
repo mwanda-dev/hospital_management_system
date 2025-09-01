3842de714cde475538cdcae57e2ed57c14cdd7ae
Alpha-Male-Dennis <dennis.chipman@cs.unza.zm>
2025-09-02 01:57:13 +0200

    Updated link in header.php

--- header.php
+++ header.php
@@ -70,7 +70,7 @@ $user = $user_result->fetch_assoc();
                             <i class="fas fa-chevron-down text-xs"></i>
                         </button>
                         <div id="userMenu" class="hidden absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-20">
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50">Profile</a>
+                            <a href="profile.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50">Profile</a>
                             <a href="settings.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50">Settings</a>
                             <a href="includes/logout.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50">Logout</a>
                         </div>
